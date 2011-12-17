e4c0ff515170bc61c987f6d4691587447623cc64
semu <sebastian@hazelcode.com>
2011-12-17 16:59:34 +0000

    Moved example to separate project

--- example.js
+++
@@ -1,9 +0,0 @@
-var kickstart = require('./index.js').withConfig({'name': 'rapdb.de', 'port': 8585, 'path': __dirname});
-var srv = kickstart.srv();
-
-srv.all('*', function(req, res) {
-  res.render('home', {title: 'node-kickstart'});
-});
-
-var router = kickstart.listen();
-console.log("Listening on http://%s:%d", kickstart.conf().name, router.address().port);
